d2a160a5f3a6fb63f612baaddb892a20eedc60cf
PogodinVyacheslav <121228358+PogodinVyacheslav@users.noreply.github.com>
2024-07-18 00:38:39 +0300

    Update main.py

--- FastAPI/Project/main.py
+++ FastAPI/Project/main.py
@@ -12,26 +12,6 @@ def getDBCon():
     conn = psycopg2.connect(DB_URL)
     return conn
 
-@app.post("/newTables/")
-async def newTables():
-    conn = getDBCon()
-    cur = conn.cursor()
-    cur.execute('CALL "Create_Main_Tables"();')
-    conn.commit()
-    cur.close()
-    conn.close()
-    return {"message": "Таблицы созданы"}
-
-@app.post("/impEmpl/")
-async def impEmpl(filepath: str):
-    conn = getDBCon()
-    cur = conn.cursor()
-    cur.execute('CALL "Create_Employees"(%s::text);',[filepath])
-    conn.commit()
-    cur.close()
-    conn.close()
-    return {"message": "Сотрудники импортированы"}
-
 @app.post("/searchEmpl/")
 async def searchEmpl(search: EmplSearch):
     conn = getDBCon()
@@ -41,16 +21,6 @@ async def searchEmpl(search: EmplSearch):
     cur.close()
     conn.close()
     return json.loads(result)
-
-@app.post("/impCoord/")
-async def impCoord(filepath: str):
-    conn = getDBCon()
-    cur = conn.cursor()
-    cur.execute('CALL "Cor_Import"(%s::text);',[filepath])
-    conn.commit()
-    cur.close()
-    conn.close()
-    return {"message": "Координаты импортированы"}
 
 if __name__ == "__main__":
     import uvicorn
